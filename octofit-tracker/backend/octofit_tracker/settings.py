a93de50b590eec8a4bcf34566aef28c5e0b4b3d8
louieGHubAcct <louie.m.labrador@accenture.com>
2026-03-15 07:13:00 +0000

    Update API URLs to use automatic-rotary-phone-jjxrjjqjq6jv35xpg and support codespace/localhost in ALLOWED_HOSTS

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-&vihs16*#c%1%nross$$e4^(pb%@+@=&sz(#gizk2#l93o9h#*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
